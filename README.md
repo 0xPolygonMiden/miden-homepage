145cdbc46fd81d1256d70fc23f8fb279ac3b36c3
Giel <g.cobben@gmail.com>
2025-03-13 06:44:43 +0100

    empty repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# Miden Homepage (DRAFT)
-This is the initial draft for the Miden homepage.
-
-##
-run `npm run build && npm run start`
--- src/components/common/SubTitle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SubTitle({texts}) {
-  return (
-    <p className='text-primaryGray text-[18px] leading-[29px] text-center'>{texts}</p>
-  )
-}
